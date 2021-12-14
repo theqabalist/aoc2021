4209e3a2e9764bdca8f6585837810e0a71c3679e
Brandon Keown <me@brandonkeown.com>
2021-12-14 11:10:57 -0600

    Day 14

---
+++ src/Days/Day14.hs
@@ -0,0 +1,54 @@
+module Days.Day14 where
+
+import Control.Monad.State (State, evalState, execState, get, put)
+import Data.Foldable (foldl')
+import Data.Map (Map, adjust, empty, findWithDefault, fromList, insert, lookup, singleton, toList, unionWith)
+import Data.Maybe (fromMaybe)
+import Data.Text (Text)
+import Days.Day14.Day14Input
+import Debug.Trace
+import Prelude hiding (lookup)
+
+frequency :: forall a. Ord a => [a] -> Map a Int
+frequency =
+  foldl'
+    ( \m v ->
+        let count = findWithDefault 0 v m
+         in insert v (count + 1) m
+    )
+    empty
+
+freqMinMax :: forall a. Map a Int -> (Int, Int)
+freqMinMax m = minmax' (toList m)
+  where
+    minmax' :: [(a, Int)] -> (Int, Int)
+    minmax' (first : rest) =
+      let minimum = foldr (\x mn -> min mn (snd x)) (snd first) rest
+          maximum = foldr (\x mx -> max mx (snd x)) (snd first) rest
+       in (maximum, minimum)
+    minmax' [] = error "cannot minmax an empty map"
+
+countDescend :: Map String Char -> Int -> String -> State (Map (String, Int) (Map Char Int)) (Map Char Int)
+countDescend mapping depth input | depth == 0 = do
+  cache <- get
+  let updated = fromMaybe (frequency input) $ lookup (input, depth) cache
+  put (insert (input, depth) updated cache)
+  pure updated
+countDescend mapping depth input@(first : next : rest) = do
+  cache <- get
+  let insertion = fromMaybe (error ("string '" <> [first, next] <> "' not found in lookup")) $ lookup [first, next] mapping
+  leftCount <- maybe (countDescend mapping (depth - 1) [first, insertion]) pure (lookup ([first, insertion], depth - 1) cache)
+  rightCount <- get >>= (maybe (countDescend mapping (depth - 1) [insertion, next]) pure . lookup ([insertion, next], depth - 1))
+  let noDoublesLeft = adjust (subtract 1) insertion leftCount
+  let noDoublesRight = adjust (subtract 1) next rightCount
+  updated <- (\rem -> foldr (unionWith (+)) empty [noDoublesLeft, noDoublesRight, rem]) <$> countDescend mapping depth (next : rest)
+  get >>= put . insert (input, depth) updated
+  pure updated
+countDescend _ _ [c] = pure (singleton c 1)
+countDescend _ _ [] = pure empty
+
+partOne :: Day14Input -> Int
+partOne (Day14Input (tmpl, mapping)) = uncurry (-) . freqMinMax $ evalState (countDescend mapping 10 tmpl) empty
+
+partTwo :: Day14Input -> Int
+partTwo (Day14Input (tmpl, mapping)) = uncurry (-) . freqMinMax $ evalState (countDescend mapping 40 tmpl) empty
---
+++ src/Days/Day14/Day14Input.hs
@@ -0,0 +1,27 @@
+module Days.Day14.Day14Input where
+
+import Data.Attoparsec.Text (Parser, endOfInput, endOfLine, letter, many1, parseOnly, string)
+import Data.Either (fromRight)
+import Data.Map (Map, fromList)
+import Data.Text (Text)
+import Lib (Parseable (parse))
+import Prelude
+
+newtype Day14Input = Day14Input (String, Map String Char)
+  deriving (Show)
+
+template :: Parser String
+template = many1 letter <* endOfLine
+
+mapping :: Parser ((Char, Char), Char)
+mapping = (,) <$> (((,) <$> letter <*> letter) <* string " -> ") <*> (letter <* endOfLine)
+
+day14Input :: Parser Day14Input
+day14Input = do
+  tmpl <- template <* endOfLine
+  mappings <- many1 mapping
+  let modified = fromList $ fmap (\((a, b), c) -> ([a, b], c)) mappings
+  pure $ Day14Input (tmpl, modified)
+
+instance Parseable Day14Input where
+  parse = fromRight (error "Invalid Input") . parseOnly day14Input
